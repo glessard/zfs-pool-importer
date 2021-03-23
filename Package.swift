b28476b24b67dfebc703ce41ac5fe6d1a54fe035
Guillaume Lessard <dhtnstff@gmail.com>
2021-03-23 16:45:48 -0600

    set minimum macOS version to 10.15 (Catalina)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "zfs-pool-importer",
+  platforms: [.macOS(.v10_15)],
   dependencies: [],
   targets: [
     .target(
